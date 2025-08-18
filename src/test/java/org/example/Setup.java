71471c9fd1857733aef197ac325010e8b952ef58
umeshpayannavar <umeshpayannavar01@gmail.com>
2025-08-18 21:41:46 +0530

    deleted one new line to test

--- src/test/java/org/example/Setup.java
+++ src/test/java/org/example/Setup.java
@@ -69,6 +69,5 @@ public class Setup extends BASETest {
         flipkartpom.verifyBTN();
         flipkartpom.scrollToText("My Activity");
         flipkartpom.homeButton();
-        flipkartpom.homeButton();
     }
 }
